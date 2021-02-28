bf98d18780a3cd03fccd8bc750bbb8cc4e7b9e53
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2021-02-28 20:00:02 -0300

    terceiro dia 22:30

---
+++ src/controllers/SurveyController.ts
@@ -0,0 +1,31 @@
+import { Response, Request } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+
+class SurveysController {
+    async create(request: Request, response: Response) {
+
+        const { title, description } = request.body;
+
+        const surveysRepository = getCustomRepository(SurveysRepository);
+
+        const survey = surveysRepository.create({
+            title,
+            description
+        });
+
+        await surveysRepository.save(survey);
+
+        return response.status(201).json(survey);
+    }
+
+    async show(request: Request, response: Response) {
+        const surveysRepository = getCustomRepository(SurveysRepository);
+
+        const allListRepository = await surveysRepository.find();
+
+        return response.json(allListRepository);
+    }
+}
+
+export { SurveysController };
